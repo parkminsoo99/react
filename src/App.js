90be611b62ad2794cc2ef689528457cccff5d8df
박민수 <zzangorc99@naver.com>
2020-12-05 14:31:21 +0000

    BooleanComponent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BooleanComponent from './03/BooleanComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <b>지루할 때 :</b>
-          <BooleanComponent bored />
-        </div>
-        <div>
-          <b>즐거울 때 :</b>
-          <BooleanComponent />
-        </div>
-      </div>
-    );
-  }
+class App extends React.Component {
+	render() {
+		return (
+		<div>
+			<div><b>지루할 때 :</b><BooleanComponent bored /></div>
+			<div><b>즐거울 때 :</b><BooleanComponent /></div>	
+		</div>
+		);
+	}
 }
 
 export default App;
