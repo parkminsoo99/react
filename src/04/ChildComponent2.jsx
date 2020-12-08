bfac4c98c9c1aa0885ec6d061dbebc899811d758
박민수 <zzangorc99@naver.com>
2020-12-08 13:09:05 +0000

    2020-12-08_01

--- src/04/ChildComponent2.jsx
+++ src/04/ChildComponent2.jsx
@@ -5,21 +5,24 @@ class ChildComponent2 extends React.Component {
 	render(){
 		const{
 			objValue,
+			requiredStringValue,
 		} = this.props;
 		
 		return (
 		<div>
 			<div>객체값 : {String(Object.entries(objValue))}</div>	
+			<div>필수값 : {requiredStringValue}</div>
 		</div>
 		);
 	}
 }
 
-ChildComponent2.PropTypes={
+ChildComponent2.propTypes={
 	objValue : PropTypes.shape({
 		name : PropTypes.string,
 		age : PropTypes.number,
 	}),
+	requiredStringValue : PropTypes.string.isRequired,
 }
 
 export default ChildComponent2;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import BooleanComponent from './03/BooleanComponent';
+import ChildComponent2 from './04/ChildComponent2';
 
 class App extends React.Component {
 	render() {
 		return (
 		<div>
-			<div><b>지루할 때 :</b><BooleanComponent bored /></div>
-			<div><b>즐거울 때 :</b><BooleanComponent /></div>	
+			<ChildComponent2 objValue={{ age: 20 }} requiredStringValue = "문자"/>
 		</div>
 		);
 	}
