7e2a2b0cb5e1495921118bca3cfe42a96f0c8d8b
박민수 <zzangorc99@naver.com>
2020-12-23 13:47:04 +0000

    2020-12-23-jest

--- src/__test__/04/Input.test.jsx
+++ src/__test__/04/Input.test.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import Input from '../../04/Input';
 
-describe('<input>', ()=>{
-	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Input />,div);
-		ReactDOM.unmountComponentAtNode(div);
+describe('<input>', () => {
+	it('renders without crashing', ()=> {
+		expect(() => {
+			shallow(<input />);
+		}).not.toThrow();
+	});
+
+	it('has one element', () => {
+		const wrapper = shallow(<Input />);
+		expect(wrapper.length).toEqual(1);
+		expect(wrapper).toHaveLength(1);
 	});
 });
